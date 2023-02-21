fcd2a7ef808b3b7fea1126184faf665df75a3936
millerdam34 <93213567+millerdam34@users.noreply.github.com>
2023-02-21 08:09:08 -0500

    added some operator overloads and timed test

--- forwardModeAD.jl
+++ forwardModeAD.jl
@@ -24,9 +24,15 @@ Base.:*(α::Number, f::Dual) = f * α
 
 Base.:/(f::Dual, g::Dual) = Dual(f.val / g.val, (f.der * g.val - g.der * f.val) / (g.val^2))
 Base.:/(f::Dual, α::Number) = Dual(f.val / α, f.der / α)
-Base.:/(α::Number, f::Dual) = Dual(α / f.val, 1 / f.der)
+# why?
+Base.:/(α::Number, f::Dual) = Dual(α / f.val, (-α * f.der) / (f.val^2))
 
 Base.:^(f::Dual, n::Integer) = Base.power_by_squaring(f,n)
+Base.:^(f::Dual, n::Integer) = Dual(f.val^n, n * (f.val ^ (n - 1)) * f.der)
+
+Base.:log(f::Dual) = Dual(log(f.val), f.der/f.val)
+
+Base.:sin(f::Dual) = Dual(sin(f.val), cos(f.val) * f.der)
 
 # Define exponential function for Dual data type
 exp(f::Dual) = Dual(exp(f.val), exp(f.val) * f.der)
@@ -68,3 +74,59 @@ a=Dual(1,0)
 b=Dual(2,0)
 c=Dual(3,1)
 d=a*(b+c)^2
+
+# Quotient Example
+a=Dual(1,0)
+b=Dual(2,1)
+
+d=a/b
+d1=1/b
+d2=a/2
+
+# power rule test
+
+f(x,y)=(x+y)^10
+
+a=Dual(1,1)
+b=Dual(1,0)
+
+Base.:^(f::Dual, n::Integer) = Dual(f.val^n, n * (f.val ^ (n - 1)) * f.der)
+
+@time f(a,b)
+
+Base.:^(f::Dual, n::Integer) = Base.power_by_squaring(f,n)
+
+@time f(a,b)
+
+# Example 
+
+@time begin
+x_1 = Dual(1.0,1.0)
+x_2 = Dual(2.0,0.0)
+
+y=log(x_1)+x_1*x_2-sin(x_2)
+
+# ∂y/∂x_1
+y.der
+
+x_1 = Dual(1.0,0.0)
+x_2 = Dual(2.0,1.0)
+
+y=log(x_1)+x_1*x_2-sin(x_2)
+
+# ∂y/∂x_2
+y.der
+
+end
+
+@time begin
+    x_1 = Dual(1.0,1.0)
+    x_2 = Dual(2.0,0.0)
+    y1=log(x_1)+x_1*x_2-sin(x_2)
+    x_1 = Dual(1.0,0.0)
+    x_2 = Dual(2.0,1.0)
+    y2=log(x_1)+x_1*x_2-sin(x_2)
+    return y1.der, y2.der
+end
+
+@time test1()
